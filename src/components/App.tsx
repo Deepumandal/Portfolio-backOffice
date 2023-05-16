85b20c656b896f7d04f425f5d35e84912351ea2c
Deepak mandal <nomercyhunters@gmail.com>
2023-05-16 14:19:52 +0530

    react ts webpack setup

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from '@mui/material';
+
+const App = () => {
+    return (
+        <div>App
+            <br />
+            <Button variant="text">Text</Button>
+            <Button variant="contained">Contained</Button>
+            <Button variant="outlined">Outlined</Button>
+        </div>
+    )
+}
+
+export default App
